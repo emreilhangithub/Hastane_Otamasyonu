029e53a12eb7b69b0a2729dec28b3c9a7dc3b76d
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-01 16:57:58 +0300

    Hasta kayıt ekranı tamamlandı

---
+++ Hastane_Otamasyonu/FrmHastaKayit.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Hastane_Otamasyonu
+{
+    partial class FrmUyeKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.MskTc = new System.Windows.Forms.MaskedTextBox();
+            this.BtnKayitYap = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CmbCinsiyet = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // MskTelefon
+            // 
+            this.MskTelefon.Location = new System.Drawing.Point(204, 202);
+            this.MskTelefon.Mask = "(999) 000-0000";
+            this.MskTelefon.Name = "MskTelefon";
+            this.MskTelefon.Size = new System.Drawing.Size(234, 42);
+            this.MskTelefon.TabIndex = 22;
+            // 
+            // MskTc
+            // 
+            this.MskTc.Location = new System.Drawing.Point(204, 141);
+            this.MskTc.Mask = "00000000000";
+            this.MskTc.Name = "MskTc";
+            this.MskTc.Size = new System.Drawing.Size(234, 42);
+            this.MskTc.TabIndex = 21;
+            this.MskTc.ValidatingType = typeof(int);
+            // 
+            // BtnKayitYap
+            // 
+            this.BtnKayitYap.BackColor = System.Drawing.Color.Lime;
+            this.BtnKayitYap.Location = new System.Drawing.Point(204, 394);
+            this.BtnKayitYap.Name = "BtnKayitYap";
+            this.BtnKayitYap.Size = new System.Drawing.Size(234, 46);
+            this.BtnKayitYap.TabIndex = 20;
+            this.BtnKayitYap.Text = "Kayıt Yap";
+            this.BtnKayitYap.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 35);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 35);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "TC Kimlik No:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 35);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Ad:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(204, 19);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(234, 42);
+            this.TxtAd.TabIndex = 24;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(204, 80);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(234, 42);
+            this.TxtSoyad.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 35);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Soyad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 35);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Telefon";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(204, 263);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(234, 42);
+            this.TxtSifre.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 35);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Cinsiyet:";
+            // 
+            // CmbCinsiyet
+            // 
+            this.CmbCinsiyet.FormattingEnabled = true;
+            this.CmbCinsiyet.Items.AddRange(new object[] {
+            "Erkek",
+            "Kadın"});
+            this.CmbCinsiyet.Location = new System.Drawing.Point(204, 324);
+            this.CmbCinsiyet.Name = "CmbCinsiyet";
+            this.CmbCinsiyet.Size = new System.Drawing.Size(234, 43);
+            this.CmbCinsiyet.TabIndex = 30;
+            // 
+            // FrmUyeKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(537, 456);
+            this.Controls.Add(this.CmbCinsiyet);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MskTelefon);
+            this.Controls.Add(this.MskTc);
+            this.Controls.Add(this.BtnKayitYap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmUyeKayit";
+            this.Text = "FrmHastaKayit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox MskTelefon;
+        private System.Windows.Forms.MaskedTextBox MskTc;
+        private System.Windows.Forms.Button BtnKayitYap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CmbCinsiyet;
+    }
+}
