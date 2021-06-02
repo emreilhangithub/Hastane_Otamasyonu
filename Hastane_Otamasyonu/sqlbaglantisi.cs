174126e109f8304bf31585d0e8e07089bc5e7519
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-02 15:05:52 +0300

    Sql bağlantı için bir sınıf oluşturduk

---
+++ Hastane_Otamasyonu/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; //kutuphaneyi ekledik
+
+namespace Hastane_Otamasyonu
+{
+    class sqlbaglantisi //sınıf
+    {
+        public SqlConnection baglanti()  //method
+        { //baglan nesne
+            SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-E9UTSVL;Initial Catalog=Hastane_Otamasyonu;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
