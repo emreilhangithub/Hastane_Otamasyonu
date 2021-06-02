050782218d68fbb29ae66c4abc43491dc37971d6
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-02 14:12:15 +0300

    sekreter giris paneli oluşturuldu

---
+++ Hastane_Otamasyonu/FrmSekreterGiris.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Hastane_Otamasyonu
+{
+    partial class FrmSekreterGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtSifre = new System.Windows.Forms.MaskedTextBox();
+            this.MskTc = new System.Windows.Forms.MaskedTextBox();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(265, 198);
+            this.TxtSifre.Mask = "00000000000";
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(234, 42);
+            this.TxtSifre.TabIndex = 39;
+            this.TxtSifre.ValidatingType = typeof(int);
+            // 
+            // MskTc
+            // 
+            this.MskTc.Location = new System.Drawing.Point(265, 132);
+            this.MskTc.Name = "MskTc";
+            this.MskTc.Size = new System.Drawing.Size(234, 42);
+            this.MskTc.TabIndex = 38;
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.Location = new System.Drawing.Point(338, 269);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(161, 46);
+            this.BtnGirisYap.TabIndex = 37;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 35);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 35);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "TC Kimlik No:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(45, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(565, 80);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Sekreter Giriş Paneli";
+            // 
+            // FrmSekreterGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(618, 339);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.MskTc);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Corbel", 14F);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmSekreterGiris";
+            this.Text = "FrmSekreter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox TxtSifre;
+        private System.Windows.Forms.MaskedTextBox MskTc;
+        private System.Windows.Forms.Button BtnGirisYap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
